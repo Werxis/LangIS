1203cef9d77c3381f62c19bcf2f0867dbc91464d
Marián Svitek <marian.svitek99@gmail.com>
2023-04-27 20:25:22 +0200

    added index file for form components

---
+++ src/components/forms/index.ts
@@ -0,0 +1,8 @@
+import Checkbox from './Checkbox';
+import FileInput from './FileInput';
+import RadioGroup from './RadioGroup';
+import SingleSelect from './SingleSelect';
+import TextArea from './TextArea';
+import TextInput from './TextInput';
+
+export { Checkbox, FileInput, RadioGroup, SingleSelect, TextArea, TextInput };
