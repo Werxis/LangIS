e6ed8c1e82a64e5bbab2328aa9a57a7ea3c65b6c
Marián Svitek <marian.svitek99@gmail.com>
2023-05-12 22:07:34 +0200

    deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['language-planet.svg', 'langis_preview.png'],
+      manifest: {
+        name: 'LangIS',
+        short_name: 'LangIS',
+        description:
+          'Website for language schools developed as an assignment in React development course.',
+        icons: [
+          {
+            src: '/language-planet.svg',
+            type: 'image/svg+xml',
+            sizes: '48x48 192x192 512x512',
+          },
+          {
+            src: '/langis_preview.png',
+            type: 'image/png',
+            sizes: '1024x683',
+          },
+          {
+            src: '/langis_preview.png',
+            type: 'image/png',
+            sizes: '1024x683',
+            purpose: 'any maskable',
+          },
+        ],
+        background_color: '#000000',
+        theme_color: '#0d47a1',
+      },
+    }),
+  ],
+});
